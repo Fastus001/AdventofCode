d65f1ffd59b1473da8e151b84061523436f06cce
Fastus <fastus6666@gamil.com>
2020-11-04 12:55:46 +0100

    Day12 - zrobione git status

--- src/com/karczmarzyk/advent2015/Day12/JSAbacusFramework.java
+++ src/com/karczmarzyk/advent2015/Day12/JSAbacusFramework.java
@@ -1,41 +1,81 @@
 package com.karczmarzyk.advent2015.Day12;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 public class JSAbacusFramework {
     public static final Pattern NUMS = Pattern.compile("(-?\\d+)");
+    public static final Pattern FIRST = Pattern.compile("^\\{");
+    public static final Pattern LAST = Pattern.compile("\\}$");
     public static List<String> tablice = new ArrayList<String>();
     public static List<String> obiekty = new ArrayList<String>();
     public static void main(String[] args) throws IOException {
-//        String tekst = new String(Files.readAllBytes(Path.of("src/com/karczmarzyk/advent2015/resources/day12.txt")));
+        String tekst = new String(Files.readAllBytes(Path.of("src/com/karczmarzyk/advent2015/resources/day12.txt")));
 
         //TODO - zaczytywaæ nawiasy klamrowe od "nasjstarszgo'!!!sdfsdfsd
-        String tekst = "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}";
-        String nowy = "";
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < tekst.length(); i++) {
-            if(tekst.charAt(i) == '[')
-                start = i;
-            if(tekst.charAt(i) == ']'){
-                end = i+1;
-                nowy = tekst.substring(start,end);
-                tekst = tekst.replace(nowy,"");
-            }
+//        String tekst = "{1,[\"c\":\"red\",\"b\":2]red,3}";
+        String result = isRedInSubstring(tekst);
+        while(result.contains("{"))
+        {
+            result = isRedInSubstring(result);
         }
-        System.out.println("nowy = " + nowy);
-        System.out.println("tekst = " + tekst);
+        System.out.println("result = " + result);
+
+        int sumPart2 = NUMS.matcher(result).results()
+                .map(MatchResult::group)
+                .mapToInt(s -> Integer.parseInt(s))
+                .sum();
+        System.out.println("sum = " + sumPart2);
 
 
-        //PART 1
+//        //PART 1
 //        int sum = NUMS.matcher(tekst).results()
 //                .map(MatchResult::group)
 //                .mapToInt(s -> Integer.parseInt(s))
 //                .sum();
 //        System.out.println("sum = " + sum);
 
+    }
+
+
+    private static String isRedInSubstring(String input) {
+        boolean toCancel = false;
+        int start = 0;
+        int end = 0;
+        int counter = 0;
+        StringBuilder tempSb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == 'r' && counter==1)
+            {
+                System.out.println("i prze³¹czenie na true= " + i + " counter = " + counter);
+                toCancel = true;
+            }
+            if(input.charAt(i) == '{' && counter==0)
+            {
+                start = i;
+                counter++;
+            }else if((input.charAt(i) == '{' || input.charAt(i)=='[') && counter !=0)
+                counter++;
+            if((input.charAt(i) == '}' || input.charAt(i) == ']') && counter !=0)
+                counter--;
+            if(input.charAt(i) == '}' && counter==0){
+                end = i+1;
+                tempSb.append(input.substring(start,end));
+                System.out.println(toCancel + " tempSb = " + tempSb);
+                input = input.replace(tempSb,"");
+                tempSb.replace(0,0+1,"[");
+                tempSb.replace(tempSb.length()-1,tempSb.length(),"]");
+                if(toCancel)
+                    return input;
+                else
+                    return tempSb.append(input).toString();
+            }
+        }
+        return input+"bbbbb";
     }
 }
