29f5b7db73eea337a66178c8c934442d24fdc6fb
Fastus <fastus6666@gamil.com>
2020-11-26 09:00:53 +0100

    Day 24 part 1 done

--- src/com/karczmarzyk/advent2015/Day24/Balance.java
+++ src/com/karczmarzyk/advent2015/Day24/Balance.java
@@ -3,24 +3,53 @@ package com.karczmarzyk.advent2015.Day24;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Balance {
-    public static List<Integer> NUMBERS = new ArrayList<Integer>();
+    public static final List<Long> NUMBERS = new ArrayList<>();
     public static final int K = 6;
 
 
     public static void main(String[] args) {
         Balance.getInput();
         Generator balance = new Generator(NUMBERS);
-        List<int[]> combinations = balance.generate(K);
-        for(int[] in :combinations)
-        {
-            System.out.println( "Arrays.toString( in = " + Arrays.toString( in ));
+        List<List<Long>> combinations = balance.generate(K);
+
+        List<List<Long>> result = combinations.stream()
+                .filter( Balance::checkIfPossibleToPlacePackages )
+                .collect( Collectors.toList() );
+//        for(List<Long> in :result)
+//        {
+//            System.out.println( "Arrays.toString( in = " + in );
+//        }
+        System.out.println( "result = " + result.size() );
+        long minQuantumEnt = result.stream()
+                .mapToLong( Balance::quantumEntanglement )
+                .min()
+                .orElseThrow();
+        System.out.println( "minQuantumEnt = " + minQuantumEnt );
+
+
+    }
+
+    private static long quantumEntanglement(List<Long> list)
+    {
+        return list.stream().reduce( ((aLong, aLong2) -> aLong*aLong2)).orElseThrow();
+    }
+
+    private static boolean checkIfPossibleToPlacePackages(List<Long> list)
+    {
+        List<Long> result = Balance.removeNumbers( list);
+        Generator balanceAfter = new Generator(result);
+        List<List<Long>> combinationsAfter;
+        for (int i = 3; i < result.size(); i++) {
+            combinationsAfter = balanceAfter.generate(i);
+            if(combinationsAfter.size()>0)
+                return true;
         }
-        System.out.println( "combinations.size() = " + combinations.size() );
+        return false;
     }
 
     private static void getInput() {
@@ -28,11 +57,19 @@ public class Balance {
         {
             while(in.hasNext())
             {
-                NUMBERS.add( in.nextInt() );
+                NUMBERS.add( in.nextLong() );
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<Long> removeNumbers(List<Long> numbers)
+    {
+        List<Long> temp = new ArrayList<>( NUMBERS );
+        temp.removeAll( numbers );
+
+        return temp;
     }
 
 }
--- src/com/karczmarzyk/advent2015/Day24/Generator.java
+++ src/com/karczmarzyk/advent2015/Day24/Generator.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Generator {
-    private final List<Integer> numbers;
+    private final List<Long> numbers;
 
 
-    public Generator(List<Integer> numbers) {
+    public Generator(List<Long> numbers) {
         this.numbers = numbers;
     }
 
-    private void helper(List<int[]> combinations, int[] data, int start, int end, int index)
+    private void helper(List<List<Long>> combinations, Long[] data, int start, int end, int index)
     {
         if(index== data.length)
         {
-            int[] combination = data.clone();
-            int sum = Arrays.stream( combination).sum();
+            List<Long> combination = List.of(data);
+            Long sum = combination.stream().reduce( Long::sum ).orElseThrow();
             if (sum==520){
                 combinations.add(combination);
             }
@@ -28,10 +28,10 @@ public class Generator {
         }
     }
 
-    public List<int[]> generate(int k)
+    public List<List<Long>> generate(int k)
     {
-        List<int[]> combinations = new ArrayList<>();
-        helper(combinations, new int[k], 0, numbers.size() -1,0);
+        List<List<Long>> combinations = new ArrayList<>();
+        helper(combinations, new Long[k], 0, numbers.size() -1,0);
         return combinations;
     }
 }
