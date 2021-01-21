f1b533b23b5db15fcddd567e07ac04b941efaddb
Fastus <fastus6666@gamil.com>
2021-01-21 14:41:57 +0100

    2016 - Day 10 part two done!

--- src/main/java/com/karczmarzyk/advent2016/day10/BotFactory.java
+++ src/main/java/com/karczmarzyk/advent2016/day10/BotFactory.java
@@ -1,14 +1,17 @@
 package com.karczmarzyk.advent2016.day10;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BotFactory {
     private static final Pattern VALUE = Pattern.compile( "value (\\d+).+(bot \\d+)" );
-    private Map<String,Bot> botMap = new HashMap<>();
+    private final Map<String,Bot> botMap = new HashMap<>();
 
     public void addBots(List<Bot> bots){
         for (Bot bot : bots) {
@@ -16,7 +19,7 @@ public class BotFactory {
         }
     }
 
-    public void addValues(List<String> values){
+    public void addValueInstructions(List<String> values){
         for (String value : values) {
             Matcher matcher = VALUE.matcher( value );
             if( matcher.find() ){
@@ -29,21 +32,24 @@ public class BotFactory {
         if(botMap.containsKey( name )){
             Bot bot = botMap.get( name );
             bot.addValue(value);
-
         }
     }
 
-    public String getBotNumber(int a, int b){
-        return botMap.values().stream().filter( bot -> bot.getValues().contains( a ) && bot.getValues().contains( b ) )
-                .findFirst().orElseThrow().getName();
+    public String getBotName(int chipA, int chipB){
+        return botMap.values()
+                .stream()
+                .filter( bot -> bot.getValues().contains( chipA ) && bot.getValues().contains( chipB ) )
+                .findFirst()
+                .orElseThrow()
+                .getName();
     }
 
     public void checkBots(){
-        botMap.values().stream().filter( Bot::isBothValues )
-                .forEach( bot -> {
-                    addValue( bot.getLow(), bot.getValue(true) );
-                    addValue( bot.getHigh(), bot.getValue(false) );
-                    });
+        botMap.values()
+                .stream()
+                .filter( Bot::isBothValues )
+                .forEach( sendOutput() );
+
         long count = botMap.values()
                 .stream()
                 .filter( bot -> !bot.isBothValues() )
@@ -52,5 +58,17 @@ public class BotFactory {
         if(count>0){
             checkBots();
         }
+    }
+
+    @NotNull
+    private Consumer<Bot> sendOutput() {
+        return bot -> {
+            addValue( bot.getLow(), bot.getValue( true ) );
+            addValue( bot.getHigh(), bot.getValue( false ) );
+        };
+    }
+
+    public Map<String, Bot> getBotMap() {
+        return botMap;
     }
 }
--- src/test/java/com/karczmarzyk/advent2016/day10/BotFactoryTest.java
+++ src/test/java/com/karczmarzyk/advent2016/day10/BotFactoryTest.java
@@ -6,40 +6,44 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BotFactoryTest {
     BotFactory factory;
 
-
     @BeforeEach
     void setUp() throws IOException {
         DataConverter converter = new DataConverter();
         converter.setInputList( Files.readAllLines(
                 Path.of( "src/main/resources/2016/day10/input.txt" )));
+
         factory = new BotFactory();
         factory.addBots( converter.getBotList() );
-        factory.addValues( converter.getValues() );
+        factory.addValueInstructions( converter.getValues() );
     }
 
     @Test
-    void addBots() {
-    }
+    void checkPartTwo() {
+        factory.checkBots();
+        Map<String, Bot> botMap = factory.getBotMap();
 
-    @Test
-    void addValues() {
-    }
+        Bot bot88 = botMap.get( "bot 88" );
+        Bot bot188 = botMap.get( "bot 188" );
+        Bot bot116 = botMap.get( "bot 116" );
 
-    @Test
-    void addValue() {
+        int result = bot88.getValue( true ) * bot116.getValue( true ) * bot188.getValue( true );
+
+
+        assertEquals( 55637,result );
     }
 
     @Test
     void checkPartOne() {
         factory.checkBots();
 
-        String botNumber = factory.getBotNumber( 61, 17 );
+        String botNumber = factory.getBotName( 61, 17 );
 
         assertEquals( "bot 147",botNumber );
     }
