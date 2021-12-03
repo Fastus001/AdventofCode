4612ab2bb246ead3c78a124459bd70584fa81aad
Fastus001 <tkarczmarzyk@gmail.com>
2021-12-03 22:03:26 +0100

    2021 - Day3 part two done, ugly

--- src/main/java/com/karczmarzyk/advent2021/day3/BinaryDiagnostic.java
+++ src/main/java/com/karczmarzyk/advent2021/day3/BinaryDiagnostic.java
@@ -1,15 +1,19 @@
 package com.karczmarzyk.advent2021.day3;
 
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Setter
 public class BinaryDiagnostic {
     private List<String> input = new ArrayList<>();
 
-    public int computePowerConsumtion() {
+    public int computePowerConsumption() {
         var sb = new StringBuilder();
         for (int i = 0; i < input.get(0).length(); i++) {
             int result = 0;
@@ -27,7 +31,53 @@ public class BinaryDiagnostic {
         String epsilon = gammaRate.replace("0", "2")
                 .replace("1", "0")
                 .replace("2", "1");
-        return Integer.parseInt(gammaRate,2) * Integer.parseInt(epsilon,2);
+        return Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilon, 2);
+    }
+
+    public List<String> bitCriteria(List<String> input, int index, boolean mostCommon) {
+        if(input.size()==1)
+            return input;
+        int result = 0;
+        for (String s : input) {
+            if (s.charAt(index) == '1') {
+                result++;
+            } else {
+                result--;
+            }
+        }
+        List<String> afterFiltering;
+        if (mostCommon){
+            afterFiltering = mostCommon(input, index, result);
+        } else {
+            afterFiltering = leastCommon(input, index, result);
+        }
+        return bitCriteria(afterFiltering, ++index, mostCommon);
+    }
+
+    @NotNull
+    private List<String> mostCommon(List<String> input, int index, int result) {
+        if(result >=0){
+            return input.stream()
+                    .filter(s -> s.charAt(index) == '1')
+                    .collect(toList());
+        } else {
+            return input.stream()
+                    .filter(s -> s.charAt(index) == '0')
+                    .collect(toList());
+        }
+    }
+
+    @NotNull
+    private List<String> leastCommon(List<String> input, int index, int result) {
+        if(result >= 0){
+            return input.stream()
+                    .filter(s -> s.charAt(index) == '0')
+                    .collect(toList());
+        } else {
+            return input.stream()
+                    .filter(s -> s.charAt(index) == '1')
+                    .collect(toList());
+        }
     }
 
 
--- src/test/java/com/karczmarzyk/advent2021/day3/BinaryDiagnosticTest.java
+++ src/test/java/com/karczmarzyk/advent2021/day3/BinaryDiagnosticTest.java
@@ -21,7 +21,7 @@ class BinaryDiagnosticTest {
         BinaryDiagnostic bd = new BinaryDiagnostic();
         bd.setInput(testInput);
 
-        int gammaRate = bd.computePowerConsumtion();
+        int gammaRate = bd.computePowerConsumption();
 
         Assertions.assertThat(gammaRate).isEqualTo(198);
     }
@@ -32,8 +32,36 @@ class BinaryDiagnosticTest {
         BinaryDiagnostic bd = new BinaryDiagnostic();
         bd.setInput(testInput);
 
-        int gammaRate = bd.computePowerConsumtion();
+        int gammaRate = bd.computePowerConsumption();
 
         Assertions.assertThat(gammaRate).isEqualTo(2498354);
+    }
+
+    @Test
+    void bitCriteria() throws IOException {
+        List<String> testInput = Files.readAllLines(Path.of("src/main/resources/2021/day3/test3.txt"));
+        BinaryDiagnostic bd = new BinaryDiagnostic();
+        bd.setInput(testInput);
+
+        String oxRating = bd.bitCriteria(testInput, 0, true).get(0);
+        String co2Rating = bd.bitCriteria(testInput, 0, false).get(0);
+
+
+        Assertions.assertThat(oxRating).isEqualTo("10111");
+        Assertions.assertThat(co2Rating).isEqualTo("01010");
+    }
+
+    @Test
+    void partTwo() throws IOException {
+        List<String> testInput = Files.readAllLines(Path.of("src/main/resources/2021/day3/input.txt"));
+        BinaryDiagnostic bd = new BinaryDiagnostic();
+        bd.setInput(testInput);
+
+        String oxRating = bd.bitCriteria(testInput, 0, true).get(0);
+        String co2Rating = bd.bitCriteria(testInput, 0, false).get(0);
+        int result = Integer.parseInt(oxRating, 2) * Integer.parseInt(co2Rating, 2);
+
+
+        Assertions.assertThat(result).isEqualTo(3277956);
     }
 }
